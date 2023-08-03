dba13c92e96ea77c789ce1a19e163d266f57ce7a
jossafossa <joosthobma@gmail.com>
2023-08-04 00:43:08 +0200

    asd

--- dockerfile
+++ dockerfile
@@ -1,14 +1,18 @@
-# Use a PHP base image
+# Use a pre-built PHP-FPM with Nginx image
 FROM php:latest
 
-# Set the working directory inside the container
-WORKDIR /var/www/html/
+# Install Nginx
+RUN apt-get update && \
+    apt-get install -y nginx
+
+# Remove the default Nginx configuration
+RUN rm /etc/nginx/sites-available/default
 
 # Copy your PHP files to the container
 COPY . /var/www/html/
 
-# Expose the port your API will run on (change it to the actual port your API listens on)
+# Expose the port for Nginx
 EXPOSE 80
 
-# Start the PHP server when the container runs
-CMD ["php", "-S", "127.0.0.1:80"]
+# Start Nginx and PHP-FPM when the container runs
+CMD service nginx start && php-fpm
--- preview.php
+++ preview.php
@@ -18,6 +18,58 @@ if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
 ?>
 
 <?php
+// formats local urls
+// ./image.png -> <domain>/image.png
+// /image.png -> <domain>/image.png
+// image.png -> <domain>/image.png
+// https://www.google.com/image.png -> https://www.google.com/image.png
+
+function formatLocalUrl($baseURL, $url) {
+  if (!$url) {
+    return null;
+  }
+
+  // strip the files from the url
+  $scheme = parse_url($baseURL, PHP_URL_SCHEME);
+  $host = parse_url($baseURL, PHP_URL_HOST);
+  $path = parse_url($baseURL, PHP_URL_PORT);
+  $port = parse_url($baseURL, PHP_URL_PORT);
+  $baseURL = `$scheme://$host:$port/$path`;
+
+  //http or https
+  if (preg_match('/^https?:\/\//i', $url)) {
+    return $url;
+  } else if (preg_match('/^\//i', $url)) {
+    return $baseURL . $url;
+  } else {
+    return $baseURL . '/' . $url;
+  }
+}
+
+
+// Sanitize the string
+// &amp; -> &
+function sanitize($str) {
+  return html_entity_decode($str);
+}
+
+function get_attribute($input_html, $match_tag, $match_attribute, $match_value, $the_attribute_to_return) {
+  $pattern = sprintf(
+    '/<%s(?=[^>]*\b%s\s*=\s*["\']%s["\'])(?=[^>]*\b%s\s*=\s*["\'](.*?)["\'])[^>]*>/i',
+    preg_quote($match_tag),
+    preg_quote($match_attribute),
+    preg_quote($match_value),
+    preg_quote($the_attribute_to_return)
+  );
+
+  if (preg_match($pattern, $input_html, $matches)) {
+    return sanitize($matches[1]);
+  } else {
+    return null;
+  }
+}
+
+
 function getPreviewData($url) {
   $data = array(
     'title' => '',
@@ -25,6 +77,7 @@ function getPreviewData($url) {
     'metaDescription' => '',
     'url' => $url,
   );
+
 
   // Set the appropriate headers
   $options = array(
@@ -42,17 +95,42 @@ function getPreviewData($url) {
   // Extract title
   $titleRegex = '/<title>(.*?)<\/title>/i';
   preg_match($titleRegex, $html, $titleMatch);
-  $data['title'] = $titleMatch ? $titleMatch[1] : '';
+  $title = $titleMatch ? $titleMatch[1] : '';
+  $data['title'] = sanitize($title);
 
   // Extract meta description
   $metaDescRegex = '/<meta\s+name=["\']description["\']\s+content=["\'](.*?)["\']/i';
   preg_match($metaDescRegex, $html, $metaDescMatch);
-  $data['metaDescription'] = $metaDescMatch ? $metaDescMatch[1] : '';
+  $metaDescription = $metaDescMatch ? $metaDescMatch[1] : '';
+  $data['metaDescription'] = sanitize($metaDescription);
 
   // Extract thumbnail
-  $thumbnailRegex = '/<meta\s+property=["\']og:image["\']\s+content=["\'](.*?)["\']/i';
-  preg_match($thumbnailRegex, $html, $thumbnailMatch);
-  $data['thumbnail'] = $thumbnailMatch ? $thumbnailMatch[1] : '';
+  // $pattern = '/<meta(?=[^>]*\bproperty\s*=\s*["\']og:image["\'])(?=[^>]*\bcontent\s*=\s*["\'](.*?)["\'])[^>]*>/i';
+  // preg_match($pattern, $html, $thumbnailMatch);
+  // $data['thumbnail'] = formatLocalUrl($url, $thumbnailMatch ? $thumbnailMatch[1] : '');
+  $thumbnails = [
+    get_attribute($html, 'meta', 'property', 'og:image', 'content'),
+    get_attribute($html, 'meta', 'itemprop', 'image', 'content'),
+  ];
+  $data['thumbnails'] = $thumbnails;
+  $thumbnails = array_values(array_filter($thumbnails));
+  $data['thumbnail'] = formatLocalUrl($url, $thumbnails[0]);
+
+
+  // get icon
+  $iconRegex = '/<link\s+rel=["\']icon["\']\s+href=["\'](.*?)["\']/i';
+  preg_match($iconRegex, $html, $iconMatch);
+  $icon = formatLocalUrl($url, $iconMatch ? $iconMatch[1] : '');
+  $icons = [
+    get_attribute($html, 'link', 'rel', 'apple-touch-icon', 'href'),
+    $icon,
+    get_attribute($html, 'link', 'rel', 'shortcut icon', 'href'),
+    get_attribute($html, 'link', 'rel', 'shortcut', 'href'),
+    get_attribute($html, 'link', 'rel', 'icon', 'href'),
+  ];
+  $data['debug'] = $icons;
+  $icons = array_values(array_filter($icons));
+  $data['icon'] = formatLocalUrl($url, $icons[0]);
 
   return $data;
 }
